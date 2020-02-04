a3affed3022cee5f0ea862854e555d690ad4d3e9
Bachelerie Luc <42067561+luckarts@users.noreply.github.com>
2020-02-04 17:13:36 +0100

    Update config.js

--- serveur/config/config.js
+++ serveur/config/config.js
@@ -8,13 +8,3 @@
 		logging: true,
 	},
 }; */
-module.exports = {
-	development: {
-		username: 'root',
-		password: 'Ghyclmsbp83^',
-		database: 'auth',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-		logging: true,
-	},
-};
